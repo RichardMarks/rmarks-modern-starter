ab3673464b9442d37dc7de2101082f7869c74963
Richard Marks <ccpsceo@gmail.com>
2015-12-28 11:20:52 -0600

    added example test

---
+++ test/tests.js
@@ -0,0 +1,20 @@
+import {describe, it, expect} from './imports';
+import {Stage} from '../src/main';
+
+describe('Stage', () => {
+    it('is available', () => {
+        expect(Stage).not.to.be.null;
+    });
+
+    let canvasElement = document.getElementById('gamecanvas');
+    let stage = new Stage(canvasElement);
+
+    it('is instantiable', () => {
+        expect(stage).not.to.be.null;
+    });
+
+    it('has the proper internal size', () => {
+        expect(stage.width).to.equal(960);
+        expect(stage.height).to.equal(540);
+    });
+});
